6e7b5042a4acb582166791a77b72d7a70464488f
A-V-Sherstnev <jdjdudjhd@gmail.com>
2026-04-16 16:04:29 +0400

    upd

--- c_cpp/memory/lesson.c
+++ c_cpp/memory/lesson.c
@@ -1,33 +1,9 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int item;
-    printf(
-        "1. Learning C language\n"
-        "2. Learning Python language\n"
-        "3. Learning Java language\n"
-        "4. Learning C++ language\n"
-        "5. Exit\n"
-    );
-    if (scanf("%d", &item) != 1) {printf("INPUT ERROR\n"); return 0;}
+int main(){
+    unsigned int var;
+    if (scanf("%u", &var) != 1) {printf("INPUT ERROR\n"); return 0;}
 
-    switch(item){
-        case 1:
-            printf("Learning C language\n");
-            break;
-        case 2:
-            printf("Learning Python language\n");
-            break;
-        case 3:
-            printf("Learning Java language\n");
-            break;
-        case 4:
-            printf("Learning C++ language\n");
-            break;
-        case 5:
-            return 0;
-    }
-    printf("Nice One!\n");
+    printf("%s\n", (var / 1000 == 3) ? "yes" : "no");
     return 0;
 }
